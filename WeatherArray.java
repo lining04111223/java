d7975f94fdba2d61facce098f74597b96c97f30f
Ning Li <lining04111223@gmail.com>
2023-01-18 23:13:06 -0800

    type

--- WeatherArray.java
+++ WeatherArray.java
@@ -1,5 +1,14 @@
 public class WeatherArray { 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        String name ="Mike";
+        
+        System.out.println(name);//print
+        int num =100;
+        final int num2 =200;//can't change
+        System.out.print(num2);
+
+        double num3 =9.9;
+        int intNum =(int)num3;
+        System.out.print(intNum);
     }
 }
