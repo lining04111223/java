e5d93b717c000f94ee3ea302030f98c66c860226
Ning Li <lining04111223@gmail.com>
2023-01-22 18:30:33 -0800

    string,array

---
+++ ArrayPractice.java
@@ -0,0 +1,21 @@
+import java.util.Arrays;
+
+public class ArrayPractice {
+
+public static void main(String[] args) {
+
+int [] numbers = new int [5];
+numbers[0]=1;
+System.out.println(numbers);
+
+System.out.println(Arrays.toString(numbers));
+
+ ///
+ int [] numbers2 = {4,2,5,1,3};
+ System.out.println(numbers2.length);
+ Arrays.sort(numbers2);
+ System.out.println(Arrays.toString(numbers2));
+ 
+
+}
+}
---
+++ String1.java
@@ -0,0 +1,14 @@
+
+public class String1 {
+  public static void main(String[] args) {
+
+String message = "Hello world" + "!!!";
+
+
+    System.out.println(message);
+    System.out.println(message.length());
+    System.out.println(message.replace("!","#"));
+
+  }
+  
+}
